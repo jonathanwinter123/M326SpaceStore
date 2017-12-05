a630e62e470f835512fdbd14f2a63b9fee50a2bb
Maverick <Jonathan_Winter@hotmail.com>
2017-12-05 13:35:34 +0100

    initial solution commit

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SpaceStorium
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new
+                              {
+                                  controller = "ProductOverview",
+                                  action = "ProductOverview",
+                                  id = UrlParameter.Optional
+                              });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "ProductDetail", action = "ProductDetail", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
