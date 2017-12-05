45bdab8172b3902ae26d57161b571633fd06f7b3
Maverick <Jonathan_Winter@hotmail.com>
2017-12-05 13:46:51 +0100

    basic formatting

---
+++ Controllers/ProductOverviewController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpaceStorium.Controllers
+{
+    public class ProductOverviewController : Controller
+    {
+        public ActionResult ProductOverview()
+        {
+            return this.View();
+        }
+    }
+}
